26ababd834c9aaf1bce5f2b02f955b32de263f09
Neotesco <albanezkarim@gmail.com>
2026-05-27 10:56:00 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // ── js/supabase.js ──
 const SUPABASE_URL = 'https://izswmkpkijkshicvhrhv.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_mTdGX2PCaORZnEoBzOp5rg_jA34zrGz';
+const SUPABASE_PUBLISHABLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6c3dta3BraWprc2hpY3Zocmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMDYzODcsImV4cCI6MjA5NDg4MjM4N30.BIabNBpDd262_NmWb04Op8zWFGYNjI2gdYVV9iNO-QU';
 
 // Client public (votes)
 // On utilise "sbClient" pour éviter le conflit avec window.supabase exposé par le CDN
